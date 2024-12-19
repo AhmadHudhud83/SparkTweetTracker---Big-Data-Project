8aa1fa2ebe8887fa3b849d44d510e02811e27b72
bayan7abdalhq <s12114374@stu.najah.edu>
2024-12-20 01:58:14 +0200

    version change

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,65 @@
-import "./App.css";
-import { useState, useEffect } from "react";
-import Axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
-  const [tweets, setTweets] = useState([]);
-  useEffect(() => {
-    Axios.get("http://localhost:5000/tweets")
-      .then((res) => setTweets(res.data))
+  const [tweets, setTweets] = useState([]); 
 
-      .catch((e) => console.error(e));
+  
+  const customIcon = new L.Icon({
+    iconUrl: require('./images/icon.png'), 
+    iconSize: [25, 30],
+    iconAnchor: [17.5, 45],
+    popupAnchor: [0, -45],
+  });
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/tweets');
+        const result = await response.json();
+        setTweets(result); 
+      } catch (error) {
+        console.error('Error fetching tweets:', error);
+      }
+    };
+
+    fetchData();
   }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        {tweets.map((tweet) => {
-          return (
-            <div>
-              <h3>{tweet.username}</h3>
-              <h6>{tweet.text}</h6>
-            </div>
-          );
-        })}
+      <MapContainer center={[32.806671, -86.791130]} zoom={5} style={{ width: '100%', height: '100vh' }}>
+      <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-      </header>
+        
+        {tweets.map((tweet) => {
+          const coordinates = tweet.geo_location?.coordinates;
+          if (coordinates && coordinates.length === 2) {
+            return (
+              <Marker
+                key={tweet._id}
+                position={[coordinates[1], coordinates[0]]} 
+                icon={customIcon}
+              >
+                <Popup>
+                  <div>
+                    <h3>Tweet ID: {tweet.tweet_id}</h3>
+                    <p>{tweet.text}</p>
+                    <p><strong>Time:</strong> {new Date(tweet.timestamp).toLocaleString()}</p>
+                  </div>
+                </Popup>
+              </Marker>
+            );
+          }
+          return null;
+        })}
+      </MapContainer>
     </div>
   );
 }
