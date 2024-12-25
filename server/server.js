9b4281ee9f4a75591fc2b5799df33f7da257fe8a
momenraddad <momenraddad12@gmail.com>
2024-12-25 20:28:11 +0200

    Add endpoint to fetch tweets by country

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 // Importing Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-//
-
+const TweetModel = require('./models/Tweets.js')
 const app = express();
 const cors  = require("cors")
 app.use(cors())
+
+const options = {
+  provider: 'openstreetmap', 
+};
 
 
 mongoose.connect('mongodb+srv://ahmad:ahmad1212@cluster0.aq5ou.mongodb.net/SparkTweetsStorage?retryWrites=true&w=majority&appName=Cluster0', 
@@ -13,17 +16,33 @@ mongoose.connect('mongodb+srv://ahmad:ahmad1212@cluster0.aq5ou.mongodb.net/Spark
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Connection error:', err));
 
-const TweetModel = require('./models/Tweets.js')
+app.get("/tweets", async (req, res) => {
+  try {
+    const tweets = await TweetModel.find().lean();;
+    res.json(tweets);
+  } catch (error) {
+    console.error("Error fetching tweets:", error);
+    res.status(500).send("Error fetching tweets");
+  }
+});
+
+app.get("/tweets/country/:countryName", async (req, res) => {
+  try {
+    const countryName = req.params.countryName; 
+    const tweetsByCountry = await TweetModel.find({ country: countryName }).lean();
+
+    if (tweetsByCountry.length === 0) {
+      return res.status(404).json({ message: `No tweets found for country: ${countryName}` });
+    }
+
+    res.json(tweetsByCountry); 
+  } catch (error) {
+    console.error("Error fetching tweets by country:", error);
+    res.status(500).send("Error fetching tweets by country");
+  }
+});
 
 
-app.get("/tweets",async (req,res)=>{
-    const tweets = await TweetModel.find();
-    console.log(tweets)
-    res.json(tweets)
-})
-
-
-//Server Config
 
 PORT_= "5000";
 app.listen(PORT_, () => {
