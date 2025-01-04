e0ab33bf07318a618c1958517079b202c61b71a3
AhmadHudhud83 <151885597+AhmadHudhud83@users.noreply.github.com>
2025-01-04 02:51:29 +0200

    edited the Tweet Schema

--- server/models/TweetModel.js
+++ server/models/TweetModel.js
@@ -1,11 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const TweetSchema = new mongoose.Schema({
-    text:{
-        type:String,
-    }
-})
-const TweetModel = mongoose.model("tweets",TweetSchema)
+    text: { type: String, required: true },
+    timestamp: { type: Date, required: true },
+    geo: {
+        type: { type: String, enum: ['Point'], default: null },
+        coordinates: { type: [Number], default: null }
+    },
+    hashtags: { type: [String], default: [] },
+    sentimentScore: { type: Number, default: null }
+}, { timestamps: true });
 
+const TweetModel = mongoose.model("tweets", TweetSchema);
 
-module.exports = TweetModel
+module.exports = TweetModel;
